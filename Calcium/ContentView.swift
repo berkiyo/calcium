6af40897bb430a27a1d9150f3c5a38bfd86d3f85
berkiyo <berk@tekbyte.net>
2021-07-18 14:35:22 +1000

    Initial setup

--- Calcium/ContentView.swift
+++ Calcium/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+/**
+ Defining the calculator buttons
+ */
+enum CalcButton {
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case zero
+    case add
+    case subtract
+    case multiply
+    case divide
+    case equal
+    case decimal
+    case percent
+    case negative
+}
+
 struct ContentView: View {
+    
+    let buttons: [[CalcButton]] = []
     var body: some View {
-        Text("Calcium")
-            .padding()
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                // Display text
+                HStack {
+                    Spacer()
+                    Text("0").bold().font(.system(size: 64)).foregroundColor(.white)
+                }
+                .padding()
+                // Calculator buttons
+            }
+        }
     }
 }
 
