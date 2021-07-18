60a3342897fe4628197ebd8ca3b708d6ff5e821e
berkiyo <berk@tekbyte.net>
2021-07-18 15:18:27 +1000

    Alpha build is ready

--- Calcium/ContentView.swift
+++ Calcium/ContentView.swift
@@ -1,15 +1,12 @@
 //
 //  ContentView.swift
-//  Calcium
+//  Calculator
 //
-//  Created by Berk Dogan on 18/7/21.
+//  Created by Afraz Siddiqui on 3/5/21.
 //
 
 import SwiftUI
 
-/**
- Defining the calculator buttons
- */
 enum CalcButton: String {
     case one = "1"
     case two = "2"
@@ -23,64 +20,145 @@ enum CalcButton: String {
     case zero = "0"
     case add = "+"
     case subtract = "-"
+    case divide = "÷"
     case multiply = "x"
-    case divide = "/"
     case equal = "="
     case clear = "AC"
     case decimal = "."
     case percent = "%"
     case negative = "-/+"
+
+    var buttonColor: Color {
+        switch self {
+        case .add, .subtract, .multiply, .divide, .equal:
+            return .orange
+        case .clear, .negative, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+        }
+    }
+}
+
+enum Operation {
+    case add, subtract, multiply, divide, none
 }
 
 struct ContentView: View {
-    
-    /**
-     Defining the buttons and positioning
-     */
+
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
+
     let buttons: [[CalcButton]] = [
-        [.clear, .negative, .percent],
-        [.seven, .eight, .nine],
-        [.four, .five, .six],
-        [.one, .two, .three],
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal],
     ]
-    
+
     var body: some View {
         ZStack {
-            Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
+            Color.black.edgesIgnoringSafeArea(.all)
+
             VStack {
-                /**
-                The result - text display
-                 */
+                Spacer()
+
+                // Text display
                 HStack {
                     Spacer()
-                    Text("0").bold().font(.system(size: 64)).foregroundColor(.white)
+                    Text(value)
+                        .bold()
+                        .font(.system(size: 100))
+                        .foregroundColor(.white)
                 }
                 .padding()
-                
-                
-                /**
-                 The buttons on the calculator
-                 */
+
+                // Our buttons
                 ForEach(buttons, id: \.self) { row in
-                    // Put them in a horizontal stack
-                    HStack {
+                    HStack(spacing: 12) {
                         ForEach(row, id: \.self) { item in
                             Button(action: {
-                                
+                                self.didTap(button: item)
                             }, label: {
                                 Text(item.rawValue)
                                     .font(.system(size: 32))
-                                    .frame(width: 70, height: 70)
-                                    .background(Color.orange)
+                                    .frame(
+                                        width: self.buttonWidth(item: item),
+                                        height: self.buttonHeight()
+                                    )
+                                    .background(item.buttonColor)
                                     .foregroundColor(.white)
-                                    .cornerRadius(35)
+                                    .cornerRadius(self.buttonWidth(item: item)/2)
                             })
                         }
                     }
+                    .padding(.bottom, 3)
                 }
             }
         }
+    }
+
+    func didTap(button: CalcButton) {
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation {
+                case .add: self.value = "\(runningValue + currentValue)"
+                case .subtract: self.value = "\(runningValue - currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear:
+            self.value = "0"
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
+
+    func buttonWidth(item: CalcButton) -> CGFloat {
+        if item == .zero {
+            return ((UIScreen.main.bounds.width - (4*12)) / 4) * 2
+        }
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+
+    func buttonHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
     }
 }
 
